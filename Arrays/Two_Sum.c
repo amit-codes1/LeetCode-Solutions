0f986e60966aa18839e9e89644228840210383aa
Amit <amit.kumar.dev08@gmail.com>
2026-08-03 20:28:28 +0530

    Added Array Programs

---
+++ Arrays/Two_Sum.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+
+    printf("Enter Number of Elements to be Stored in the Array: ");
+    scanf("%d", &n);
+
+    int *arr = (int *)malloc(n * sizeof(int));
+
+    if (arr == NULL)
+    {
+        printf("Memory Allocation Failed!!");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Enter Number: ");
+        scanf("%d", &arr[i]);
+    }
+
+    int target;
+
+    printf("Enter Target Value: ");
+    scanf("%d", &target);
+
+    int index = -1;
+    int index1 = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+
+        for (int j = i + 1; j < n; j++)
+        {
+
+            if ((arr[j] + arr[i]) == target)
+            {
+
+                index = j;
+                index1 = i;
+                break;
+            }
+        }
+    }
+
+    printf("The indexes are: %d and %d", index1, index);
+    
+    free(arr);
+
+    arr = NULL;
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# LeetCode DSA Solutions in C
+
+This repository contains my **LeetCode Data Structures and Algorithms (DSA)** solutions implemented in **C**.
+
+## Topics Covered:
+- Array
+
+## Programs:
+
+### Arrays:
+- Two Sum
